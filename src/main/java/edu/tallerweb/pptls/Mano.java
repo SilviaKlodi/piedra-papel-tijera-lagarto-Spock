65c1d26bc4deded5a3d4155907578d16faa01a45
SilviaKlodi <silviaklodi@yahoo.com.ar>
2013-09-28 19:33:11 -0300

    Trabajo Practico Resuelto

--- src/main/java/edu/tallerweb/pptls/Mano.java
+++ src/main/java/edu/tallerweb/pptls/Mano.java
@@ -4,14 +4,15 @@ package edu.tallerweb.pptls;
  * Representa una de las Manos involucradas en el juego
  */
 public class Mano {
-
+	Forma valor;
 	/**
 	 * Toda Mano debe crearse con una forma dada, que será
 	 * la que determine su condición en el juego.
 	 * @param forma, la Forma que adopta la Mano.
 	 */
 	public Mano(final Forma forma) {
-		throw new RuntimeException("No implementado aún");
+		this.valor = forma;
+		//Diefinir Constructor//throw new RuntimeException("No implementado aún");
 	}
 
 	/**
@@ -21,7 +22,34 @@ public class Mano {
 	 * @return un Resultado, de acuerdo al estado del juego.
 	 */
 	public Resultado jugarCon(final Mano otra) {
-		throw new RuntimeException("No implementado aún");
+		
+		if (this.valor == Forma.PIEDRA && (otra.valor == Forma.LAGARTO || otra.valor == Forma.TIJERA))
+			return Resultado.GANA;
+		if (this.valor == Forma.PIEDRA && (otra.valor == Forma.SPOCK || otra.valor == Forma.PAPEL))
+			return Resultado.PIERDE;
+		
+		if (this.valor == Forma.SPOCK && (otra.valor == Forma.TIJERA || otra.valor == Forma.PIEDRA))
+			return Resultado.GANA;
+		if (this.valor == Forma.SPOCK && (otra.valor == Forma.PAPEL || otra.valor == Forma.LAGARTO))
+			return Resultado.PIERDE;
+		
+		if (this.valor == Forma.PAPEL && (otra.valor == Forma.PIEDRA || otra.valor == Forma.SPOCK))
+			return Resultado.GANA;
+		if (this.valor == Forma.PAPEL && (otra.valor == Forma.TIJERA || otra.valor == Forma.LAGARTO))
+			return Resultado.PIERDE;
+		
+		if (this.valor == Forma.LAGARTO && (otra.valor == Forma.PAPEL || otra.valor == Forma.SPOCK))
+			return Resultado.GANA;
+		if (this.valor == Forma.LAGARTO && (otra.valor == Forma.TIJERA || otra.valor == Forma.PIEDRA))
+			return Resultado.PIERDE;
+
+		if (this.valor == Forma.TIJERA && (otra.valor == Forma.PAPEL || otra.valor == Forma.LAGARTO))
+			return Resultado.GANA;
+		if (this.valor == Forma.TIJERA && (otra.valor == Forma.SPOCK || otra.valor == Forma.PIEDRA))
+			return Resultado.PIERDE;
+		
+		return Resultado.EMPATA;
+			//throw new RuntimeException("No implementado aún");
 	}
 
 }
--- src/test/java/edu/tallerweb/pptls/PartidaTests.java
+++ src/test/java/edu/tallerweb/pptls/PartidaTests.java
@@ -16,4 +16,100 @@ public class PartidaTests {
 
 	}
 
+	@Test
+	public void queSpockDestrozaTijera() {
+
+		Mano jugadorUno = new Mano(Forma.SPOCK);
+		Mano jugadorDos = new Mano(Forma.TIJERA);
+
+		assertEquals("Piedra aplasta Lagarto",
+				Resultado.GANA, jugadorUno.jugarCon(jugadorDos));
+
+	}
+	@Test
+	public void quePiedraAplastaLagarto() {
+
+		Mano jugadorUno = new Mano(Forma.PIEDRA);
+		Mano jugadorDos = new Mano(Forma.LAGARTO);
+
+		assertEquals("Piedra aplasta Lagarto",
+				Resultado.GANA, jugadorUno.jugarCon(jugadorDos));
+
+	}
+
+	@Test
+	public void quePiedraAplastaTijera() {
+
+		Mano jugadorUno = new Mano(Forma.PIEDRA);
+		Mano jugadorDos = new Mano(Forma.TIJERA);
+
+		assertEquals("Piedra aplasta Tijera",
+				Resultado.GANA, jugadorUno.jugarCon(jugadorDos));
+
+	}
+
+	@Test
+	public void quePapelRefutaSpock() {
+
+		Mano jugadorUno = new Mano(Forma.PAPEL);
+		Mano jugadorDos = new Mano(Forma.SPOCK);
+
+		assertEquals("Papel refuta Spock",
+				Resultado.GANA, jugadorUno.jugarCon(jugadorDos));
+
+	}
+	@Test
+	public void quePapelCubrePiedra() {
+
+		Mano jugadorUno = new Mano(Forma.PAPEL);
+		Mano jugadorDos = new Mano(Forma.PIEDRA);
+
+		assertEquals("Papel cubre Piedra",
+				Resultado.GANA, jugadorUno.jugarCon(jugadorDos));
+
+	}
+
+	@Test
+	public void queLagartoEnvenenaSpock() {
+
+		Mano jugadorUno = new Mano(Forma.LAGARTO);
+		Mano jugadorDos = new Mano(Forma.SPOCK);
+
+		assertEquals("Lagarto envenena Spock",
+				Resultado.GANA, jugadorUno.jugarCon(jugadorDos));
+
+	}
+
+	@Test
+	public void queLagartoComePapel() {
+
+		Mano jugadorUno = new Mano(Forma.LAGARTO);
+		Mano jugadorDos = new Mano(Forma.PAPEL);
+
+		assertEquals("Lagarto come Papel",
+				Resultado.GANA, jugadorUno.jugarCon(jugadorDos));
+
+	}
+
+	@Test
+	public void queTijeraCortaPapel() {
+
+		Mano jugadorUno = new Mano(Forma.TIJERA);
+		Mano jugadorDos = new Mano(Forma.PAPEL);
+
+		assertEquals("Tijera corta Papel",
+				Resultado.GANA, jugadorUno.jugarCon(jugadorDos));
+
+	}
+
+	@Test
+	public void queTijeraDecapitaLagarto() {
+
+		Mano jugadorUno = new Mano(Forma.TIJERA);
+		Mano jugadorDos = new Mano(Forma.LAGARTO);
+
+		assertEquals("Tijera decapita Lagarto",
+				Resultado.GANA, jugadorUno.jugarCon(jugadorDos));
+
+	}
 }
